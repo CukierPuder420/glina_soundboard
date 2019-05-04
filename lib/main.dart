224fca02a4cd4848bb51ab81c8f3aee1d04a9bfa
CukierPuder420 <kuba.krapiec@outlook.com>
2019-05-04 11:23:49 +0200

    app rotation is now fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:glina_soundboard/ekrany/splashscreen.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(GlinaSoundboard());
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(GlinaSoundboard());
+}
 
 class GlinaSoundboard extends StatelessWidget {
 	@override
